6c18133eec760c099a86fb90c253eda32bc3ed3f
narayansinghjai <jainarayansingh.b@gmail.com>
2018-09-05 10:35:02 +0000

    jav.c

---
+++ sample.c
@@ -0,0 +1,28 @@
+void main()
+class Get
+{
+void read()
+{
+System.out.println("read java not");
+practices();
+}
+void practices()
+{
+Sytem.out.println("practices the programs");
+getjob();
+}
+void getjob()
+{
+System.out.println("got the job");
+}
+}
+class Operations
+{
+    public static void main(string[] args)
+{
+Get go=new Get();
+go.read();
+}
+}
+
+
